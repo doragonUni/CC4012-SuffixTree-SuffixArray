959985d115cf7f0aa975cf33150560263c3af866
javierLavados <javierlavadosjilbert@gmail.com>
2021-12-04 17:53:41 -0300

    added the first part

--- suffix_array.py
+++ suffix_array.py
@@ -21,3 +21,25 @@ then we sort the elements
 and the suffix array is completed as shown:
 10-7-0-3-5-8-1-4-6-9-2
 '''
+
+#[10, 7, 0, 3, 5, 8, 1, 4, 6, 9, 2]
+
+import sys
+
+#if len(sys.argv) != 2:
+  #  print('Use: '+sys.argv[0]+' archivo.txt')
+    #sys.exit(1)
+s='banana'
+
+#Momentanio, solucion sacada de google
+
+txt = open(sys.argv[1])
+fulltext = txt.read()
+
+# recordar cambiarlo
+def suffixArray(s):
+    suffixes = [(s[i:], i) for i in range(len(s))] # o(n)
+    suffixes.sort(key=lambda x: x[0]) #nlog(n)
+    return [s[1] for s in suffixes] #o(n)
+
+print(suffixArray(fulltext))
