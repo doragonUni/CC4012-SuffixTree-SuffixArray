3aba2683fcf460098a705c18953d34ec3b007b9e
doragonUni <lung.pang@ug.uchile.cl>
2021-12-06 17:52:44 -0300

    code refactoring

--- program_array.py
+++ program_array.py
@@ -1,18 +1,20 @@
 import suffix_array as sa
 import sys
 
-txt = open(sys.argv[1])
+txt = open(sys.argv[1]) #The text is given when the file is called
 fulltext = txt.read()
-array =  sa.suffixArray(fulltext)
+s_array =  sa.suffixArray(fulltext)
 
 print("------------------------------------------")
 while True:
+    print("Press Enter to exit")
     word = input("What word do you want to search?")
+    
     if word=="":
         break
-    coincidences=sa.search_all(array, word, fulltext)
-    print("We found "+str(len(coincidences))+ " coincidences","\n")
-    for c in coincidences:
+    occurrences=sa.search_all(s_array, word, fulltext)
+    print("We found "+str(len(occurrences))+ " occurrences","\n")
+    for c in occurrences:
         if c>10:
             text=fulltext[c-10:c+10]
         else:
