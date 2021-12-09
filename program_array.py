e1de8f05e8b5371ebe2f81c859dd94395a6076ec
doragonUni <lung.pang@ug.uchile.cl>
2021-12-09 01:10:29 -0300

    fixed suffix array for 1MB

--- program_array.py
+++ program_array.py
@@ -3,7 +3,8 @@ import sys
 
 txt = open(sys.argv[1]) #The text is given when the file is called
 fulltext = txt.read()
-s_array =  sa.suffixArray(fulltext)
+#s_array =  sa.suffixArray(fulltext)
+s_array = sa.buildSuffixArray(fulltext)
 print("------------------------------------------")
 while True:
     print("Press Enter to exit")
